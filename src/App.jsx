bdd6b93f851ac82775107501658dac741ba08a3e
SachinTiwari986 <tiwari2sachin@gmail.com>
2025-05-27 08:44:40 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="app">
+            <h1>Smart Task Manager</h1>
+            {/* Components like TaskInput, TaskList, TaskStats, Timer will go here */}
+        </div>
+    );
+}
+
+export default App;
